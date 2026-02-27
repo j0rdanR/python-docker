e21d3eed64efcde1e761c279639dd4c86342a156
Jordan Rodrigues <96402459+j0rdanR@users.noreply.github.com>
2026-02-27 12:43:15 +0800

    expose web server

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import cowsay
+import flask
+import torch
 
-name = input("Enter your name: ")
+app = flask.Flask(__name__)
 
-cowsay.cow(f"Hello {name} from Docker!")
+@app.route("/")
+def index():
+    msg = cowsay.get_output_string("cow", "Hello from Docker!")
+    return f"<pre><code>{msg}</code></pre>"
+
+@app.route("/gpu")
+def gpu():
+    return torch.cuda.get_device_name()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
